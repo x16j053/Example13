e2b769f810c813f435bc4e461d3b51601b8b885c
WIN_NT\x16j053 <x16j053@chiba-fjb.ac.jp>
2017-05-19 12:39:49 +0900

    while

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExec_Click(object sender, EventArgs e)
+        {
+            DialogResult result = DialogResult.Yes;
+            while (result==DialogResult.Yes)
+            {
+                result =MessageBox.Show("続けますか？" ,"確認",
+                    MessageBoxButtons.YesNo);
+                System.Threading.Thread.Sleep(1000);
+            }
+            MessageBox.Show("WHILEループを終了しました", "情報");
+        }
+    }
+}
